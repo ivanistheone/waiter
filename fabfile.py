1bc636cd81d097a9069ec05326d3af61d0174847
Arvind Srinivasan <arvind.srinivasan@gmail.com>
2017-06-05 17:29:53 -0700

    fabfile

---
+++ fabfile.py
@@ -0,0 +1,19 @@
+from fabric.api import *
+from fabric.context_managers import shell_env
+
+env.hosts = [
+    'leq.sidewayspass.com',
+]
+
+def update():
+    env.user   = "leq"
+    code_dir = '/webapps/leq/waiter'
+    with cd(code_dir):
+      with prefix("source activate"):
+        run("git pull")
+        run("pip install -r requirements/production.txt")
+
+def deploy():
+  env.user = "arvnd"
+  env.shell = "/bin/sh -c"
+  sudo("supervisorctl restart leq", pty=False)
