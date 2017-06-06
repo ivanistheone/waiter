2374ebbdce4eb96f96b08cb890be859ecee89c26
Arvind Srinivasan <arvind.srinivasan@gmail.com>
2017-06-06 12:10:26 -0700

    syntax error

--- config/urls.py
+++ config/urls.py
@@ -8,7 +8,7 @@ from django.views import defaults as default_views
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
     url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
-    url(r'^schedule/$', TemplateView.as_view(template_name='pages/schedule.html'))
+    url(r'^schedule/$', TemplateView.as_view(template_name='pages/schedule.html'), name='schedule'),
 
     # Django Admin, use {% url 'admin:index' %}
     url(settings.ADMIN_URL, admin.site.urls),
