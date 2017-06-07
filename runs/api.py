0d0f5dd81b9eca0b0fc296710e71890853035498
Ivan Savov <ivan.savov@gmail.com>
2017-06-07 10:36:35 -0400

    Simplified API code
      - Use the same serializer for both create and detail
      - Switched to generics for /channels/ since no special processing is required

--- runs/api.py
+++ runs/api.py
@@ -1,70 +1,31 @@
 
 from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import ContentChannel, ContentChannelRun, ChannelRunLog, ChannelRunEvent
-from .serializers import ContentChannelSerializer, ContentChannelCreateSerializer
-from .serializers import ContentChannelRunSerializer, ContentChannelRunCreateSerializer
-from .serializers import ChannelRunEventSerializer, ChannelRunEventCreateSerializer
+from .serializers import ContentChannelSerializer, ContentChannelRunSerializer, ChannelRunEventSerializer
 
 
+# CONTENT CHANNELS #############################################################
 
-class ContentChannelList(APIView):
+class ContentChannelListCreate(ListCreateAPIView):
     """
     List all content channels or create a new channel.
     """
-    def get_serializer_class(self):
-        return ContentChannelSerializer
+    queryset = ContentChannel.objects.all()
+    serializer_class = ContentChannelSerializer
 
-    def get(self, request, format=None):
-        """
-        List all content channels.
-        """
-        channels = ContentChannel.objects.all()
-        serializer = ContentChannelSerializer(channels, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        """
-        Register a new content channels at the sushi bar.
-        """
-        serializer = ContentChannelCreateSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class ContentChannelDetail(APIView):
+class ContentChannelDetail(RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update, or delete a content channel instance.
     """
-    def get_object(self, channel_id):
-        try:
-            return ContentChannel.objects.get(channel_id=channel_id)
-        except ContentChannel.DoesNotExist:
-            raise Http404
-
-    def get(self, request, channel_id, format=None):
-        channel = self.get_object(channel_id)
-        serializer = ContentChannelSerializer(channel)
-        return Response(serializer.data)
-
-    def put(self, request, channel_id, format=None):
-        channel = self.get_object(channel_id)
-        serializer = ContentChannelSerializer(channel, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, channel_id, format=None):
-        channel = self.get_object(channel_id)
-        channel.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
+    queryset = ContentChannel.objects.all()
+    serializer_class = ContentChannelSerializer
+    lookup_field =  'channel_id'
 
 
 
@@ -101,12 +62,11 @@ class ContentChannelRunListCreate(APIView):
         """
         Create a new channel run.
         """
-        serializer = ContentChannelRunCreateSerializer(data=request.data)
+        serializer = ContentChannelRunSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 
 class ContentChannelRunDetail(APIView):
     """
@@ -138,6 +98,8 @@ class ContentChannelRunDetail(APIView):
 
 
 
+# CHANNEL RUN EVENTS ###########################################################
+
 class ChannelRunEventListCreate(APIView):
     """
     GET:  list all events for run `run_id`
@@ -155,9 +117,8 @@ class ChannelRunEventListCreate(APIView):
         """
         Create a new channel run.
         """
-        serializer = ChannelRunEventCreateSerializer(data=request.data)
+        serializer = ChannelRunEventSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
--- runs/serializers.py
+++ runs/serializers.py
@@ -6,40 +6,16 @@ from rest_framework import serializers
 from .models import ContentChannel, ContentChannelRun, ChannelRunLog, ChannelRunEvent
 
 
+
 class ContentChannelSerializer(serializers.ModelSerializer):
     channel_id = serializers.UUIDField(format='hex')
     class Meta:
         model = ContentChannel
         fields = ('channel_id', 'name', 'description', 'version', 'source_domain', 'source_id', 'user_registered_by', 'content_server')
 
-class ContentChannelCreateSerializer(serializers.Serializer):
-    channel_id = serializers.UUIDField(format='hex')
-    name = serializers.CharField(max_length=200, allow_blank=True)  # equiv to ricecooker's `title`
-    description = serializers.CharField(max_length=400, allow_blank=True)
-    version = serializers.IntegerField(default=0)
-    source_domain = serializers.CharField(max_length=300, required=False, allow_blank=True)
-    source_id = serializers.CharField(max_length=200, required=False, allow_blank=True, allow_null=True)
-    user_registered_by = serializers.EmailField(max_length=200, required=False, allow_blank=True, allow_null=True)
-    user_token = serializers.CharField(max_length=200, required=False, allow_blank=True)
-    content_server = serializers.URLField(max_length=300, default='https://develop.contentworkshop.learningequality.org')
-
-    def create(self, validated_data):
-        """
-        Create and return a new `ContentChannel` instance, given the validated data.
-        """
-        return ContentChannel.objects.create(**validated_data)
-
-
 
 
 class ContentChannelRunSerializer(serializers.ModelSerializer):
-    run_id = serializers.UUIDField(format='hex', read_only=True)
-    channel = ContentChannelSerializer(read_only=True)
-    class Meta:
-        model = ContentChannelRun
-        fields = ('run_id', 'channel', 'chef_name', 'ricecooker_version', 'state', 'started', 'finished', 'duration')
-
-class ContentChannelRunCreateSerializer(serializers.ModelSerializer):
     run_id = serializers.UUIDField(format='hex', read_only=True)
     channel_id = serializers.UUIDField(format='hex', write_only=True)   # need to supply channel_id when creating run
     channel = ContentChannelSerializer(read_only=True)
@@ -48,11 +24,11 @@ class ContentChannelRunCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = ContentChannelRun
         read_only_fields = ('run_id', 'channel', 'state', 'started', 'finished', 'duration')
-        fields = ('run_id', 'channel_id', 'channel', 'chef_name', 'ricecooker_version')
+        fields = ('run_id', 'channel_id', 'channel', 'chef_name', 'ricecooker_version', 'state', 'started', 'finished', 'duration')
 
     def create(self, validated_data):
         """
-        Create and return a new `ContentChannel` instance, given the validated data.
+        Create and return a new `ContentChannelRun` instance, given the validated data.
         """
         channel_id = validated_data.pop('channel_id')
         channel = ContentChannel.objects.get(channel_id=channel_id)
@@ -63,27 +39,15 @@ class ContentChannelRunCreateSerializer(serializers.ModelSerializer):
 
 
 
-
-
-
-
 class ChannelRunEventSerializer(serializers.ModelSerializer):
-    run_id = serializers.UUIDField(source='run.run_id', format='hex', read_only=True)
-    class Meta:
-        model = ChannelRunEvent
-        fields = ('id', 'run_id', 'event', 'progress', 'timestamp')
-
-class ChannelRunEventCreateSerializer(serializers.ModelSerializer):
     run_id = serializers.UUIDField(format='hex')
-
     class Meta:
         model = ChannelRunEvent
-        read_only_fields = ('id', )
         fields = ('id', 'run_id', 'event', 'progress', 'timestamp')
 
     def create(self, validated_data):
         """
-        Create and return a new `ContentChannel` instance, given the validated data.
+        Create and return a new `ContentChannelRun` instance, given the validated data.
         """
         run_id = validated_data.pop('run_id')
         run = ContentChannelRun.objects.get(run_id=run_id)
