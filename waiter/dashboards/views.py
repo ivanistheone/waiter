8be207c66e652e0b6393ef82506b2203dd5df466
Arvind Srinivasan <arvind.srinivasan@gmail.com>
2017-06-08 10:35:05 -0700

    wip views

--- waiter/dashboards/views.py
+++ waiter/dashboards/views.py
@@ -24,6 +24,29 @@ class DashboardView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(DashboardView, self).get_context_data(**kwargs)
         context['keys'] = ('Active Channels', 'Inactive Channels')
+        channel_names = ['TESSA', 'Pratham', 'MIT', 'Khan Academy']
         context['channels'] = dict(zip(context['keys'], ([get_mock(), get_mock(channel='TESSA', restart_color='secondary'), get_mock(channel='Pratham', run_status='warning', num_errors=14), get_mock(channel='MIT', status_pct=50, status='Scraping')], [get_mock()])))
-        context['jschannels'] = mark_safe(','.join("\"%s\"" % s for s in ('TESSA', 'Pratham', 'MIT', 'Khan Academy')))
+        # TODO(arvnd): This can very easily be optimized by
+        # querying the runs table directly.
+        for channel in ContentChannel.objects.all():
+            # TODO(arvnd): add active bit to channel model and 
+            # split on that.
+            last_run = ContentChannelRun.objects.filter(channel__channel_id=channel.channel_id)[:1]
+            context['inactive_channels'].append({
+                    "channel": channel.name,
+                    "channel_url": "%s/%s/edit" % (channel.default_content_server, channel.channel_id),
+                    "restart_color": 'secondary',
+                    "stop_color": "secondary",
+                    "id": channel.channel_id,
+                    "last_run": datetime.strftime("%b %w, %H:%M",last_run.events[-1].finished),
+                    # do we have an overall event or do we have to sum these?
+                    "duration": "0",
+                    "status": last_run.events[-1].name,
+                    # TODO
+                    "status_pct": 100,
+                    "num_errors": 0,
+                    "run_status": "success",
+                })
+
+        context['jschannels'] = mark_safe(','.join("\"%s\"" % s for s in channel_names))
         return context
