0d0c176bbdc70e640ca202d83e45e93c3bfd0734
Arvind Srinivasan <arvind.srinivasan@gmail.com>
2017-06-15 13:48:29 -0700

    fabfile changes for gce instance

--- fabfile.py
+++ fabfile.py
@@ -4,26 +4,26 @@ from fabric.api import *
 from fabric.context_managers import shell_env
 
 env.hosts = [
-    'leq.sidewayspass.com',
+    '35.188.9.9',
 ]
 
 def pull():
     env.user   = "leq"
-    code_dir = '/webapps/leq/waiter'
+    code_dir = '/webapp/waiter'
     with cd(code_dir):
         run("git pull")
 
 def update():
     env.user   = "leq"
-    code_dir = '/webapps/leq/waiter'
+    code_dir = '/webapp/waiter'
     with cd(code_dir):
-        with prefix("source activate"):
+        with prefix("source sushibar/bin/activate"):
             run("git pull")
             run("pip install -r requirements/production.txt")
             run("./manage.py collectstatic --noinput")
 
 def deploy():
-    env.user = os.environ.get('USER')
+    env.user = "bharadwajs"
     env.shell = "/bin/sh -c"
     sudo("supervisorctl restart leq", pty=False)
     sudo("supervisorctl restart leq_channels_workers", pty=False)
